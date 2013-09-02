2b4d6c807d68a27bbf5d02c5e4c4165ded66fdcb
Peter Friedrich <peter@friedrich1988.de>
2013-09-02 15:16:25 +0200

    functions implemented

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 class Rack(object):
 	"""handles all devices and connections"""
-	def __init__(self):
-		devices = {}
+	def __init__(self, name):
+		self.name = name
+		self.devices = {}
 
 	def change(self, deviceID, paramName, newValue):
 		"""
@@ -18,6 +19,9 @@ class Rack(object):
 		"""
 
 		device = self.getDeviceByID(deviceID)
+		if not device:
+			return False
+
 		if device.changeParam(paramName, newValue):
 			return True
 		else:
@@ -36,6 +40,10 @@ class Rack(object):
 
 		return self.devices.get(devID, None)
 
+	def addDevice(self, id, device):
+		"""adds new device"""
+		self.devices[id] = device
+
 
 class Device(object):
 	"""every part of the rack"""
@@ -46,7 +54,8 @@ class Device(object):
 
 class Instrument(Device):
 	"""device that represents a hardware synthesizer"""
-	def __init__(self, arg):
+	def __init__(self, name):
+		self.name = name
 		self.parameters = {}
 		
 	def changeParam(self, paramName, value):
@@ -62,7 +71,7 @@ class Instrument(Device):
 		"""
 		param = self.getParamByName(paramName)
 		if param:
-			if param.change(value):
+			if param.setValue(value):
 				return True
 			else:
 				return False
@@ -75,16 +84,159 @@ class Instrument(Device):
 		"""
 		return self.parameters.get(paramName, None)
 
+	def addParam(self, param):
+		"""adds new param"""
+		self.parameters[param.getName()] = param
 
-class Parameter(object):
-	"""one Parameter of an instrument"""
+	def sync(self):
+		"""
+		syncs changed parameter with hardware
+		"""
+		for param in self.parameters.values():
+			if not param.isInSync():
+				self.send(param.update())
+
+
+class Utility(Device):
+	"""device that processes midi data"""
 	def __init__(self, arg):
-		super(Parameter, self).__init__()
+		super(Utilizy, self).__init__()
 		self.arg = arg
+		
 
-
-class Interface(midimanager.Utility):
-	"""device to communicate with the MIDI Adapter"""
+class Interface(Utility):
+	"""communicates with MIDI interface"""
 	def __init__(self, arg):
 		super(Interface, self).__init__()
 		self.arg = arg
+		
+
+class Parameter(object):
+	"""
+	one parameter of a device
+	
+	@todo: move getName to superclass
+	@todo: use super constructor
+	"""
+	def __init__(self, name):
+		self.value = ""
+		self.name = name
+
+	def setValue(self, newvalue):
+		"""
+		sets new value
+		"""
+		self.value = value
+
+
+class SynthParam(Parameter):
+	"""parameter for an instrument"""
+	def __init__(self, name):
+		super(SynthParam, self).__init__(name)
+		self.inSync = False
+		self.midiTemplate = ""
+
+	def update(self):
+		"""
+		returns MIDI Message to transfer current status to midi device
+		"""
+		self.setSyncFlag()
+		return self.getMIDIMessage()
+
+	def isInSync(self):
+		"""returns true if in sync with hardware"""
+		return self.inSync
+
+	def setSyncFlag(self):
+		"""sets in sync"""
+		self.inSync = True
+
+	def unsetSyncFag(self):
+		"""unset in sync"""
+		self.inSync = False
+
+	def setValue(self, newvalue):
+		"""sets new value"""
+		if self.isValid(newvalue):
+			self.value = newvalue
+			self.unsetSyncFag()
+			return True
+		else:
+			return False
+
+	def getValue(self):
+		"""gets value"""
+		return self.value
+
+	def getName(self):
+		"""gets name"""
+		return self.name
+
+	def getMIDIMessage(self):
+		"""returns midi message"""
+		return True
+
+		
+class NormalParam(SynthParam):
+	"""parameter from 0 to 127"""
+	def __init__(self, name):
+		super(NormalParam, self).__init__(name)
+		self.value = 0
+
+	def isValid(self, value):
+		"""tests if value is valid"""
+		return (0 <= value <= 127)
+
+		
+class EnumParam(SynthParam):
+	"""parameter with n different values"""
+	def __init__(self, name, valuedict):
+		super(EnumParam, self).__init__(name)
+		self.valuedict = valuedict
+		# set value randomly
+		self.value = valuedict.keys()[0]
+
+	def isValid(self, value):
+		"""tests if value is valid"""
+		return (value in self.valuedict)
+
+	def getData(self):
+		"""returns data"""
+		return self.valuedict[self.value]
+
+		
+class PanParam(SynthParam):
+	"""parameter from -64 to 63"""
+	def __init__(self, name):
+		super(PanParam, self).__init__(name)
+
+	def isValid(self, value):
+		"""tests if value is valid"""
+		return (-64 <= value <= 63)
+
+	def getData(self):
+		"""returns midi value"""
+		return self.value + 64
+
+
+class MIDIMessage(object):
+	"""midi message ready to be sent"""
+	def __init__(self, arg):
+		super(MIDIMessage, self).__init__()
+		self.arg = arg
+		
+
+
+class MIDITemplate(object):
+	"""
+	template for variable midi message
+	
+	@todo: subclasses for midi CC or PC templates	
+	"""
+	def __init__(self, arg):
+		super(MIDIMessageTemplate, self).__init__()
+		self.arg = arg
+
+	def getMessage(self, value):
+		"""makes message from value and template"""
+		pass
